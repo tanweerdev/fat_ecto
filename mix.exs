e9af515d02b1ff73d749b845abb34148cbffb98f
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-10-31 19:09:16 +0500

    Update version number for hex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FatEcto.MixProject do
   def project do
     [
       app: :fat_ecto,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
