3e75b6f4c2a951f5873fe31e89725f31c34757e0
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-11-08 13:33:20 +0500

    Update hex version for publishing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FatEcto.MixProject do
   def project do
     [
       app: :fat_ecto,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
