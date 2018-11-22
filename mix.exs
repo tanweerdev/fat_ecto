64dbe0f0d86fc6958da5672aa27eae5f6e610da6
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-11-22 10:27:24 +0500

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule FatEcto.MixProject do
   def project do
     [
       app: :fat_ecto,
-      version: "0.2.0",
-      elixir: "~> 1.6",
+      version: "0.2.1",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
