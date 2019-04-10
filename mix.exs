618c64825305d67d9d8356cf4b0797feb56fffb8
Tanweer Shahzaad <tanweerdev@gmail.com>
2019-04-11 03:05:28 +0500

    Bump version release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FatEcto.MixProject do
   def project do
     [
       app: :fat_ecto,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
