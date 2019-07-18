195c96cf0132421fa4819771de22a38e334f2fb2
Tanweer Shahzaad <tanweerdev@gmail.com>
2019-07-18 17:07:52 +0500

    Support Elixir > 1.9 only

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FatEcto.MixProject do
     [
       app: :fat_ecto,
       version: "0.4.1",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
--- test/utils/date_time_test.exs
+++ test/utils/date_time_test.exs
@@ -3,26 +3,26 @@ defmodule Utils.DateTimeTest do
   import FatUtils.DateTime
 
   test "parse! integer datetime" do
-    assert inspect(parse!(1_464_096_368)) == "#DateTime<2016-05-24 13:26:08Z>"
+    assert inspect(parse!(1_464_096_368)) == "~U[2016-05-24 13:26:08Z]"
   end
 
   test "parse! integer datetime with microseconds" do
-    assert inspect(parse!(1_432_560_368_868_569)) == "#DateTime<2015-05-25 13:26:08.868569Z>"
+    assert inspect(parse!(1_432_560_368_868_569)) == "~U[2015-05-25 13:26:08.868569Z]"
   end
 
   test "parse! binary datetime " do
-    assert inspect(parse!("2015-01-23T23:50:07Z")) == "#DateTime<2015-01-23 23:50:07Z>"
+    assert inspect(parse!("2015-01-23T23:50:07Z")) == "~U[2015-01-23 23:50:07Z]"
   end
 
   test "parse integer datetime" do
-    assert inspect(parse(1_464_096_368)) == "{:ok, #DateTime<2016-05-24 13:26:08Z>}"
+    assert inspect(parse(1_464_096_368)) == "{:ok, ~U[2016-05-24 13:26:08Z]}"
   end
 
   test "parse integer datetime with microseconds" do
-    assert inspect(parse(1_432_560_368_868_569)) == "{:ok, #DateTime<2015-05-25 13:26:08.868569Z>}"
+    assert inspect(parse(1_432_560_368_868_569)) == "{:ok, ~U[2015-05-25 13:26:08.868569Z]}"
   end
 
   test "parse binary datetime " do
-    assert inspect(parse("2015-01-23T23:50:07Z")) == "{:ok, #DateTime<2015-01-23 23:50:07Z>}"
+    assert inspect(parse("2015-01-23T23:50:07Z")) == "{:ok, ~U[2015-01-23 23:50:07Z]}"
   end
 end
