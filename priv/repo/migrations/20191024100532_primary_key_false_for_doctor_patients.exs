bf4a2865f8cb4c824c93bb142704df600316bd1c
Hammad <hammad.ahmed@regbits.org>
2019-10-24 15:55:45 +0500

    Fix and update bridge table schemas

---
+++ priv/repo/migrations/20191024100532_primary_key_false_for_doctor_patients.exs
@@ -0,0 +1,15 @@
+defmodule FatEcto.Repo.Migrations.PrimaryKeyFalseForDoctorPatients do
+  use Ecto.Migration
+
+  def change do
+    drop constraint(:fat_doctors_patients, "fat_doctors_patients_fat_doctor_id_fkey")
+    drop constraint(:fat_doctors_patients, "fat_doctors_patients_fat_patient_id_fkey")
+   alter table(:fat_doctors_patients, primary_key: false) do
+    remove(:id)
+   
+   modify(:fat_doctor_id, references(:fat_doctors), null: false, primary_key: true)
+   
+  modify(:fat_patient_id, references(:fat_patients), null: false, primary_key: true)
+   end
+  end
+end
--- test/support/fat_models/hospital_doctor_model.ex
+++ test/support/fat_models/hospital_doctor_model.ex
@@ -2,10 +2,10 @@ defmodule FatEcto.FatHospitalDoctor do
   @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
-
+  @primary_key false
   schema "fat_hospitals_doctors" do
-    belongs_to(:fat_doctor, FatEcto.FatDoctor)
-    belongs_to(:fat_hospital, FatEcto.FatHospital)
+    belongs_to(:fat_doctor, FatEcto.FatDoctor, primary_key: true)
+    belongs_to(:fat_hospital, FatEcto.FatHospital, primary_key: true)
   end
 
   def changeset(struct, params \\ %{}) do
@@ -14,5 +14,9 @@ defmodule FatEcto.FatHospitalDoctor do
       :fat_doctor_id,
       :fat_hospital_id
     ])
+    |> validate_required([:fat_doctor_id,
+    :fat_hospital_id])
+    |> foreign_key_constraint(:fat_doctor_id)
+    |> foreign_key_constraint(:fat_hospital_id)
   end
 end
