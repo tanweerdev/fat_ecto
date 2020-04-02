55f9a8af6ca489557311084a9db574063d5d8dd2
Tanweer Shahzaad <tanweerdev@gmail.com>
2020-04-02 16:58:24 +0500

    Update create macro

--- lib/fat_ecto/controller_macros/create_macro.ex
+++ lib/fat_ecto/controller_macros/create_macro.ex
@@ -34,10 +34,14 @@ defmodule FatEcto.CreateRecord do
         changeset = @schema.changeset(struct(@schema), params)
         changeset = process_changeset_before_insert(changeset, conn)
 
-        with {:ok, record} <- @repo.insert(changeset) do
+        with {:ok, record} <- insert_record(changeset, @repo) do
           record = MacrosHelper.preload_record(record, @repo, @preloads)
           render_record(conn, record, unquote(options) ++ [status_to_put: :created])
         end
+      end
+
+      def insert_record(changeset, repo) do
+        repo.insert(changeset)
       end
 
       # You can use process_params_before_in_create to override params before calling changeset
@@ -50,7 +54,7 @@ defmodule FatEcto.CreateRecord do
         changeset
       end
 
-      defoverridable process_params_before_in_create: 2, process_changeset_before_insert: 2
+      defoverridable process_params_before_in_create: 2, process_changeset_before_insert: 2, insert_record: 2
     end
   end
 end
