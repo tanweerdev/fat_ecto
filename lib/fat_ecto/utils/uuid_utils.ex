8826afb8733469071c02a5bca3758cd0a9a972c6
Tanweer Shahzaad <tanweerdev@gmail.com>
2020-04-16 13:23:00 +0500

    Update uuid utils to accept options

--- lib/fat_ecto/utils/uuid_utils.ex
+++ lib/fat_ecto/utils/uuid_utils.ex
@@ -1,9 +1,9 @@
 defmodule FatUtils.UUID do
   # TODO: add docs
   # will return {invalid_uuids, params}
-  def parse(params, list_of_uuid_fields) when is_map(params) do
+  def parse(params, list_of_uuid_fields, options \\ []) when is_map(params) do
     Enum.reduce(params, {[], %{}}, fn {key, value}, acc ->
-      if key in list_of_uuid_fields do
+      if !(value in (options[:valid_values] || [])) && key in list_of_uuid_fields do
         case Ecto.UUID.cast(value) do
           {:ok, _valid_uuid} ->
             {elem(acc, 0), Map.put(elem(acc, 1), key, value)}
