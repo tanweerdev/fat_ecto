ada6cfb494fbe8dd5688b82032171971d6417952
Muhammad Tanweer <tanweerdev@gmail.com>
2024-04-26 13:20:28 +0000

    Add sortable functionality (#60)

---
+++ test/support/my_app_room_orderby.ex
@@ -0,0 +1,20 @@
+defmodule MyApp.RoomOrderby do
+  import Ecto.Query
+
+  use FatEcto.FatQuery.Sortable,
+    allowed_fields: %{},
+    customizable_fields: %{
+      "phone" => "$desc",
+      "name" => "$asc"
+    }
+
+  def custom_orderby_fallback(query, "name", "$asc") do
+    order_by(query, [r], asc: r.name)
+  end
+
+  def custom_orderby_fallback(query, "phone", "$desc") do
+    order_by(query, [r], desc: r.phone)
+  end
+
+  def custom_orderby_fallback(query, _, _), do: query
+end
